ec133127448bd30470a8f83062ceb1c4cac15b30
briannicolasdc <briannicolasdc@gmail.com>
2023-03-29 15:37:00 -0300

    playlists updated

---
+++ src/api/playlists.js
@@ -0,0 +1,19 @@
+export default function getPlaylist(){
+    return [
+        {
+           title: 'Minha playlist n° 3',
+        },
+        {
+           title: 'Nova pasta',
+        },
+        {
+           title: 'Minha playlist n° 2',
+        },
+        {
+           title: 'RapCaviar',
+        },
+        {
+           title: 'Brian',
+        },
+    ]
+}
